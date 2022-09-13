f8517eeb0b6cf4653bdf3d69d5a1e66fca909a3c
Pablo Magalhães <109962972+pabloaugustocm17@users.noreply.github.com>
2022-09-13 09:36:48 -0300

    ADD Classe aresta #1
    Adicionado classe aresta, junto a todos seus métodos.

---
+++ codigos/Aresta.java
@@ -0,0 +1,56 @@
+package codigos;
+
+class Aresta{
+
+    private int destino;
+    private boolean visitada;
+    private int peso;
+
+    private void init(int destino, boolean visitada, int peso){
+
+        this.destino = destino;
+        this.visitada = visitada;
+        this.peso = peso;
+    
+    }
+
+    Aresta(){
+        init(-1, false, 0);
+    }
+
+    public int getDestino(){
+        return this.destino;
+    }
+
+    public int getPeso(){
+        return this.peso;
+    }
+
+    public boolean getVisitada(){
+        return this.visitada;
+    }
+
+    public void limparVisita(){
+
+        if(this.visitada){
+            this.visitada = false;
+        }else{
+            Util.ImprimiMensagem("Essa aresta não foi visitada ainda");
+        }
+
+    }
+
+    public void visitarAresta(){
+
+        if(!this.visitada){
+            this.visitada = true;
+        }else{
+            Util.ImprimiMensagem("Essa aresta já foi visitada");
+        }
+
+    }
+
+    
+
+
+}
